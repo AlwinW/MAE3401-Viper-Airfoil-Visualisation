287b11b36fbb0686378ffc54b7944b2a0ba13d81
AlwinW <alwang18@hotmail.com>
2017-03-17 01:12:47 +1100

    Created vectors for airfoil normals
    Tangents will require additional effort

--- Viper-Airfoil-Visualisation-App/Rough Tangents and Normals.R
+++ Viper-Airfoil-Visualisation-App/Rough Tangents and Normals.R
@@ -7,7 +7,6 @@ t = (NACA %% 100) / 100
 c = 1
 a = - 1/2
 
-x = 0.4
 
 AirfoilCurve <- function(x, out = "all") {
   if(x >= a & x < p + a) {
@@ -43,11 +42,29 @@ AirfoilGrads <- function(x, step = c/100000, out = "all") {
   out <- data.frame(
     surf = c("upper", "upper", "lower", "lower"),
     eq = c("tan", "norm", "tan", "norm"),
-    x = x,
+    xo = x,
+    x = with(surf, c(xU[2], xU[2], xL[2], xL[2])),
     y = with(surf, c(yU[2], yU[2], yL[2], yL[2])),
     m = c(dyUdx, -1/dyUdx, dyLdx, -1/dyLdx)) %>%
     mutate(c = -m*x + y)
+  return(out)
 }
 
 
-AirfoilLineGen <- function(x, )
+AirfoilLineGen <- function(x, gradint = AirfoilGrads(x), surf = "upper", eq = "norm", focusdist = 1, len = focusdist*50+1) {
+  # NOTE: Tan is broken, won't work!!
+  gradint <- gradint %>%
+    filter(surf == get("surf") & eq == get("eq")) %>%
+    mutate(focusdist = focusdist, len = len) %>%
+    mutate(xfocusdist = sign(m)*focusdist/sqrt(1+m^2)*ifelse(surf=="upper",1,-1)) %>%
+    mutate(xfocus = x + xfocusdist, yfocus = y + xfocusdist*m) %>%
+    mutate(xmax = x + 4*xfocusdist, ymax = y + 4*xfocusdist*m)
+  print(gradint)
+  xvec = with(gradint, c(seq(x, xfocus, length.out = len), seq(x, xmax, length.out = len)))
+  yvec = with(gradint, c(seq(y, yfocus, length.out = len), seq(y, ymax, length.out = len)))
+  return(data.frame(xvec, yvec))
+}
+
+# ggplot(AirfoilLineGen(-0.4, surf = "upper")) + 
+  # geom_point(aes(x = xvec, y = yvec))
+
