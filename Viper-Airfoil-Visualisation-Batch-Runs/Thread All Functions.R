610f0fd115a326705908fca0033fb5ac944bde97
AlwinW <alwang18@hotmail.com>
2017-04-10 00:23:20 +1000

    Initial Branch
    - Tecplot Files
    - Finer resolution for runs

--- Viper-Airfoil-Visualisation-Batch-Runs/Thread All Functions.R
+++ Viper-Airfoil-Visualisation-Batch-Runs/Thread All Functions.R
@@ -83,7 +83,7 @@ TreadAll <- function(filename, foldername, airfoildata, savedata, saveplot) {
   
   #--- Interpolation on Normals ----
   source("Function Airfoil Normals.R")    # For "AirfoilGrads", etc
-  xvec = AirfoilSamp(seq(a, a+c, by = 0.5), cylinder = TRUE)
+  xvec = AirfoilSamp(seq(a, a+c, by = 0.05), cylinder = TRUE)
   dist = NormalSamp(seq(0, 1.5, by = 0.05))
   # Find the combined lvec for interpolation
   lvec <- NormalLvec(xvec, dist, AoA)
@@ -120,7 +120,7 @@ TreadAll <- function(filename, foldername, airfoildata, savedata, saveplot) {
 
   #--- Boundary Layer Calculations ----
   source("Function Boundary Layers.R")    # For "BLCalcs", etc
-  xvec = AirfoilSamp(seq(a, a+c, by = 0.2), polyn = 5, cylinder = TRUE)
+  xvec = AirfoilSamp(seq(a, a+c, by = 0.05), polyn = 5, cylinder = TRUE)
   blvals = BLCalcs(omesh, xvec, AoA, Re)
   bltheory = BLTheory(omesh, xvec, AoA, Re) %>%
     mutate(x = xO)
@@ -190,7 +190,7 @@ TreadAll <- function(filename, foldername, airfoildata, savedata, saveplot) {
     rm(xvec, blvals, bltheory, interpnorms, plot_bl_methods, plot_bl_max, plot_bl_thick, plot_bl_maxlog)
   
   #--- Velocity Profile Calculations ----
-  sep = 0.2
+  sep = 0.1
   vec = VelProfileLvec(omesh, sep, blplot, AoA)
   xvec = vec$xvec; lvec = vec$lvec; distmax = vec$distmax; rm(vec)
   velprofile = VelProfile(blplot, xvec, lvec, omesh, AoA, Re)
